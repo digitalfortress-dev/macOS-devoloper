e77370727ee79776efe5277c7346811537d8e06d
levanluu <levan999@gmail.com>
2023-07-30 22:11:32 +0700

    chore: update install libraries

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,3 +1,17 @@
+# Running install homebrew
+install_homebrew() {
+    if ! command -v brew &> /dev/null; then
+        echo "Homebrew not found. Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "Homebrew installed successfully."
+    else
+        echo "Homebrew is already installed."
+    fi
+}
+
+install_homebrew
+#############################
+
 # Running install git
 install_git() {
     if ! command -v git &> /dev/null; then
@@ -10,6 +24,7 @@ install_git() {
 
 # Check if Git is installed
 install_git
+#####################
 
 # Check if the repository already exists, if not, clone it
 if [ ! -d "macOS-devoloper" ]; then
